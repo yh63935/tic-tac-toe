54bcee47dee1a90751c119f9d024078a5ea93fe4
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2023-10-31 20:56:31 -0700

    Create representation of gameboard using squares

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+// Gameboard that represents the board in an array
+function gameBoard() {
+    const board = [];
+    for (let row = 0; row < 3; row++) {
+        board[row] = [];
+        for(let column = 0; column < 3; column++) {
+            board[row].push(square());
+        }
+    }
+    // Add marker to array
+    const addMarker = (row, column, player) => {
+        // check if the square is available, if yes, fill square with marker
+        // if no, stop
+        const checkSquareAvailable = () => {
+            return board[row][column].getValue() === "";
+        }
+
+        if (checkSquareAvailable()) {
+            board[row][column].addMarker(player)
+        } else {
+            console.log("Pick another square")
+        }
+    }
+
+    // Print the values of the gameboard 
+    const printBoard = () => {
+        console.log(board.map(row=>row.map(square=>square.getValue())))
+    }
+
+    return {addMarker, printBoard}
+
+}
+
+const players = createPlayers();
+players[0].toggleActive();
+const gmb = gameBoard();
+gmb.addMarker(0, 1, players[0])
+gmb.printBoard();
+
+// Create individual squares on the board
+function square() {
+    let value = "";
+
+    const addMarker = (player) => {
+        value = player.marker;
+        console.log(player.marker)
+    }
+    const getValue = () => {
+        return value;
+    }
+    return {addMarker, getValue}
+}
 function createPlayers() {
     const inputOne = document.getElementById('player1');
     const inputTwo = document.getElementById('player2');
@@ -22,8 +74,7 @@ function createPlayer(name, marker) {
     }
     return {name, marker, toggleActive, getActiveStatus }
 }
-const players = createPlayers();
-players[0].toggleActive();
+
 
 function getCurrentPlayer() {
     // If playerOne has active status of true, current player is player one, else player two is the current player
