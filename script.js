f35d6d8f8d20c6640032f5c27ff754f112b4c08a
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2023-11-01 23:11:51 -0700

    Add game logic to check for row, column, or diagonal wins

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function gameBoard() {
             board[row].push(square());
         }
     }
+    const getBoard = ()=> board;
     // Add marker to array
     const addMarker = (row, column, player) => {
         // check if the square is available, if yes, fill square with marker
@@ -27,16 +28,15 @@ function gameBoard() {
         console.log(board.map(row=>row.map(square=>square.getValue())))
     }
 
-    return {addMarker, printBoard}
+    return {getBoard, addMarker, printBoard}
 
 }
 
 const players = createPlayers();
 players[0].toggleActive();
-const gmb = gameBoard();
-gmb.addMarker(0, 1, players[0])
-gmb.printBoard();
 
+
+gameController().winLose();
 // Create individual squares on the board
 function square() {
     let value = "";
@@ -49,6 +49,89 @@ function square() {
         return value;
     }
     return {addMarker, getValue}
+}
+
+// Gamecontroller manages game logic
+function gameController() {
+    // Switch players
+    // Play round
+        const gmb = gameBoard();
+        gmb.addMarker(0, 2, players[0])
+        gmb.addMarker(1, 1, players[0])
+        gmb.addMarker(2, 0, players[0])
+        // gmb.addMarker(0, 0, players[0])
+        // gmb.addMarker(1, 0, players[0])
+        // gmb.addMarker(2, 0, players[0])
+
+
+    // Check if there is a win or lose
+    const winLose = ()=> {
+        // Win cases
+        // board[0][0], board[0][1], board[0][2] are the same
+        // board[1][0], board[1][1], board[1][2] are the same
+        // board[2][0], board[2][1], board[1][2] are the same
+        const board = gmb.getBoard();
+        // Check for 3 in a row for row
+        for(let row=0; row<board.length; row++) {
+            let checkRow = [];
+            console.log(board.map((elem, index)=>elem.map((inner, i)=> board[index][i].getValue())))
+            for(let column=0; column< board[row].length; column++) {
+                checkRow.push(board[row][column].getValue())
+                console.log("test")
+            }  
+            if (checkRow.every(square=>square==="X") || checkRow.every(square=>square==="O")) {
+                console.log(checkRow)
+
+                console.log('It\'s a row win')
+                return true;
+            } ;
+        }
+
+        // Check for 3 in a row for column
+        for (let column=0; column< 3; column++) {
+            let checkCol = [];
+            for(let row=0; row < board.length; row++) {
+                checkCol.push(board[row][column].getValue());
+                console.log(board[row][column].getValue())
+            }
+
+            if (checkCol.every(square=>square ==="X") || checkCol.every(square=>square ==="O")) {
+                console.log('Column win')
+                return true;
+            } 
+        }
+
+        // Check for a diagonal win
+        for(let row=0; row<board.length; row++) {
+            let checkDiagonalOne = [];
+            for(let column=0; column< row.length; row++) {
+                if(row===column) {
+                    checkDiagonalOne.push(board[row][column].getValue())
+                }
+            }  
+            if (checkDiagonalOne.every(square=>square==="X") || checkDiagonalOne.every(square=>square==="O")) {
+                console.log('It\'s a diagonal win')
+                return true;
+            } ;
+        }
+
+        for(let row=0; row<board.length; row++) {
+            let checkDiagonalTwo = [];
+            for(let column=0; column< row.length; row++) {
+                if(row+column === board.length-1) {
+                    checkDiagonalTwo.push(board[row][column].getValue())
+                }
+            }  
+            if (checkDiagonalTwo.every(square=>square==="X") || checkDiagonalTwo.every(square=>square==="O")) {
+                console.log('It\'s a diagonal2 win')
+                return true;
+            } ;
+        }
+
+        
+    }
+    return {winLose}
+
 }
 function createPlayers() {
     const inputOne = document.getElementById('player1');
